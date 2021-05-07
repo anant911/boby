af91c2b207796fe67a52de0f2cbf0efb75512d0f
anant911 <77596400+anant911@users.noreply.github.com>
2021-05-06 21:57:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+name_of_the_student_array=[];
+
+function submit()
+{
+
+    var name1= document.getElementById("student1").value;
+    var name2= document.getElementById("student2").value;
+    var name3= document.getElementById("student3").value;
+    var name4= document.getElementById("student4").value;
+0
+
+    name_of_the_student_array.push(name1);
+    name_of_the_student_array.push(name2);
+
+    name_of_the_student_array.push(name3);
+
+    name_of_the_student_array.push(name4);
+
+    console.log(name_of_the_student_array);
+
+    document.getElementById("displayname").innerHTML=name_of_the_student_array;
+    document.getElementById("submit_button").style.display="none"
+    document.getElementById("sort_button").style.display="insine-block"    
+
+
+}
+
+function sort()
+{
+    name_of_the_student_array.sort();
+    console.log(name_of_the_student_array);
+    document.getElementById("displayname").innerHTML=name_of_the_student_array;
+}
